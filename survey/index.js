d424053b7c8e760d5ac2a96a37862e0e6f191159
msharma55578 <m.sharma55578@gmail.com>
2020-08-17 18:48:01 +0530

    Update index.js

--- survey/index.js
+++ survey/index.js
@@ -5,20 +5,24 @@ var mysql = require('mysql');
 const app=express();
 app.use(bodyParser())
 app.use(cors())
+app.get("/demo",(req,res)=>{
+  console.log("Here");
+})
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "mohit555"
+    password: "mohit555",
+    database: "mydbase"
   });
   
   con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    //con.query("CREATE DATABASE mydbase", function (err, result) {
-      //if (err) throw err;
-     // console.log("Database created");
-   // });
   });
+  
+  app.get("/",(req,res)=>{
+    res.send("Hello")
+  })
   app.get("/question",(req,res)=>{
     con.query("SELECT * FROM survey",function(err,result,fields){
       if(err) throw err;
@@ -27,8 +31,9 @@ var con = mysql.createConnection({
   })
   app.post("/question",(req,res)=>{
     let question=req.body
-    var sql="INSERT INTO customers (type,id,question,option) VALUES ('Company Inc', '37','hlw','1')";
-    console.log(request.body)
+    console.log("Question is",question)
+    var sql="INSERT INTO customers (type,id,question,options) VALUES ('Company Inc', '37','hlw','1')";
+    console.log(req.body)
     con.query(sql, function (err, result) {
     if (err) throw err;
       console.log("1 record inserted");
@@ -36,6 +41,6 @@ var con = mysql.createConnection({
   })
   
 
-app.listen('8081',() =>{
+app.listen('3008',() =>{
     console.log('running');
 })
